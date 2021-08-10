b40e848954d8da3690597e2d0cb8df38298140e1
Rudenko Stanislav <rudenkostas2@gmail.com>
2021-08-10 15:09:14 +0300

    adding modal

--- public/dino-game/script.js
+++ public/dino-game/script.js
@@ -1,9 +1,15 @@
 const character = document.getElementById("character")
 const block = document.getElementById("block")
 
-setTimeout(()=>{
-    block.classList.add("block-animation")
-}, 1000)
+const modal = document.getElementById("myModal");
+const span = document.getElementsByClassName("close")[0];
+
+const startGame = ()=>{
+    setTimeout(()=>{
+        block.classList.add("block-animation")
+    }, 1000) 
+}
+startGame()
 
 function jump(){
     if(character.classList != "animate"){
@@ -21,9 +27,24 @@ const render = setInterval(function(){
     const characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"))
     const blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"))
 
-    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
-        block.style.animation = "none"
+    if(blockLeft<20 && blockLeft>-80 && characterTop>=130){
         block.style.display = "none"
-        alert("gg")
+        block.classList.remove("block-animation")
+        modal.style.display = "block";
     }
 }, 10)
+
+
+span.onclick = function() {
+  modal.style.display = "none";
+  block.style.display = "inline"
+  startGame()
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    block.style.display = "inline"
+    startGame()
+  }
+}
