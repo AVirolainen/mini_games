fb63711b0a77f48c7c06d24c28472404eb1d515d
Rudenko Stanislav <rudenkostas2@gmail.com>
2021-08-17 02:51:45 +0300

    adding doodle game

---
+++ public/doodle-game/script.js
@@ -0,0 +1,73 @@
+const grid = document.querySelector(".grid")
+const doodler = document.createElement("div")
+
+let doodlerLeftSpace = 50
+let doodlerBottomSpace = 250
+
+let isGameOver = false
+
+let platformAmount = 5
+
+let platforms = []
+
+function createDoodler(){
+    grid.appendChild(doodler)
+    doodler.classList.add("doodler")
+
+    doodler.style.left = doodlerLeftSpace + 'px'
+    doodler.style.bottom = doodlerBottomSpace + 'px'
+
+}
+
+class Platform{
+    constructor(newPlatBottom){
+        this.bottom = newPlatBottom
+        this.left = Math.random()*315
+
+        this.visual = document.createElement('div')
+
+        const visual = this.visual
+        visual.classList.add('platform')
+        visual.style.left = this.left + 'px'
+        visual.style.bottom = this.bottom +'px'
+        grid.appendChild(visual)
+
+    }
+}
+
+function createPlatforms(){
+    for(let i=0; i<platformAmount; i++){
+        let platformGap = 600/platformAmount
+
+        let newPlatBottom = 100+(i*platformGap)
+        let newPlatform = new Platform(newPlatBottom)
+
+        platforms.push(newPlatform)
+        console.log(platforms)
+
+
+    }
+
+}
+
+function movePlatform(){
+    if(doodlerBottomSpace > 200){
+        platforms.forEach(platform=>{
+            platform.bottom -= 4
+            let visual = platform.visual
+            visual.style.bottom = platform.bottom + 'px'
+        })
+    }
+}
+
+function start(){
+    if(!isGameOver){
+        createDoodler()
+        createPlatforms()
+        setInterval(movePlatform, 30)
+        
+    }
+}
+
+start()
+
