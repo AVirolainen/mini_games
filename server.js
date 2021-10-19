b73c29ba24dc3143e73fe0816be10871e6085b15
Rudenko Stanislav <rudenkostas2@gmail.com>
2021-10-19 21:44:30 +0300

    test commit

--- server.js
+++ server.js
@@ -9,9 +9,6 @@ app.use('/',(req,res,next)=>{
         res.sendFile(path.join(__dirname, "public", 'index.html'))
 });
 
-app.get('/dinosaur-game',(req,res,next)=>{
-    res.send('hello world');
-});
 
 
 
